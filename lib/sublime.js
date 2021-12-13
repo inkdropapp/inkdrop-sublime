inkdropapp/inkdrop-sublime

commit ee8802e743ec7660f6afed33a56573967e68e584

feat(keymaps): support reverse sort lines

lib/sublime.js
@@ -420,7 +420,7 @@ module.exports = function (CodeMirror) {
     })
   }
 
-  function sortLines(cm, caseSensitive) {
+  function sortLines(cm, caseSensitive, direction) {
     if (cm.isReadOnly()) return CodeMirror.Pass
     var ranges = cm.listSelections(),
       toSort = [],
@@ -446,7 +446,10 @@ module.exports = function (CodeMirror) {
         var start = Pos(from, 0),
           end = Pos(to)
         var lines = cm.getRange(start, end, false)
-        if (caseSensitive) lines.sort()
+        if (caseSensitive)
+          lines.sort(function (a, b) {
+            return a < b ? -direction : a == b ? 0 : direction
+          })
         else
           lines.sort(function (a, b) {
             var au = a.toUpperCase(),
@@ -455,7 +458,7 @@ module.exports = function (CodeMirror) {
               a = au
               b = bu
             }
-            return a < b ? -1 : a == b ? 0 : 1
+            return a < b ? -direction : a == b ? 0 : direction
           })
         cm.replaceRange(lines, start, end)
         if (selected) ranges.push({ anchor: start, head: Pos(to + 1, 0) })
@@ -465,10 +468,16 @@ module.exports = function (CodeMirror) {
   }
 
   cmds.sortLines = function (cm) {
-    sortLines(cm, true)
+    sortLines(cm, true, 1)
+  }
+  cmds.reverseSortLines = function (cm) {
+    sortLines(cm, true, -1)
   }
   cmds.sortLinesInsensitive = function (cm) {
-    sortLines(cm, false)
+    sortLines(cm, false, 1)
+  }
+  cmds.reverseSortLinesInsensitive = function (cm) {
+    sortLines(cm, false, -1)
   }
 
   cmds.nextBookmark = function (cm) {
@@ -564,9 +573,7 @@ module.exports = function (CodeMirror) {
   }
 
   cmds.smartBackspace = function (cm) {
-    if (cm.somethingSelected()) {
-      return cm.execCommand('delCharBefore')
-    }
+    if (cm.somethingSelected()) return CodeMirror.Pass
 
     cm.operation(function () {
       var cursors = cm.listSelections()
@@ -753,7 +760,9 @@ module.exports = function (CodeMirror) {
     'Cmd-J': 'joinLines',
     'Shift-Cmd-D': 'duplicateLine',
     F5: 'sortLines',
+    'Shift-F5': 'reverseSortLines',
     'Cmd-F5': 'sortLinesInsensitive',
+    'Shift-Cmd-F5': 'reverseSortLinesInsensitive',
     F2: 'nextBookmark',
     'Shift-F2': 'prevBookmark',
     'Cmd-F2': 'toggleBookmark',
@@ -815,7 +824,9 @@ module.exports = function (CodeMirror) {
     'Ctrl-J': 'joinLines',
     'Shift-Ctrl-D': 'duplicateLine',
     F9: 'sortLines',
+    'Shift-F9': 'reverseSortLines',
     'Ctrl-F9': 'sortLinesInsensitive',
+    'Shift-Ctrl-F9': 'reverseSortLinesInsensitive',
     F2: 'nextBookmark',
     'Shift-F2': 'prevBookmark',
     'Ctrl-F2': 'toggleBookmark',

lib/index.js
@@ -79,7 +79,11 @@ class SublimePlugin {
       'sublime:join-lines': h('joinLines'),
       'sublime:duplicate-line': h('duplicateLine'),
       'sublime:sort-lines': h('sortLines'),
+      'sublime:reverse-sort-lines': h('reverseSortLines'),
       'sublime:sort-lines-insensitive': h('sortLinesInsensitive'),
+      'sublime:reverse-sort-lines-insensitive': h(
+        'reverseSortLinesInsensitive'
+      ),
       'sublime:next-bookmark': h('nextBookmark'),
       'sublime:prev-bookmark': h('prevBookmark'),
       'sublime:toggle-bookmark': h('toggleBookmark'),

commit 28f0f52101eca6b536bb2256ef590f340c38f70f

fix(keymap): smartBackspace not working
It's a regression by ee8802e743ec7660f6afed33a56573967e68e584

lib/sublime.js
@@ -573,7 +573,7 @@ module.exports = function (CodeMirror) {
   }
 
   cmds.smartBackspace = function (cm) {
-    if (cm.somethingSelected()) return CodeMirror.Pass
+    if (cm.somethingSelected()) return cm.execCommand('delCharBefore')
 
     cm.operation(function () {
       var cursors = cm.listSelections()